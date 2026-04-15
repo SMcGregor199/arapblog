925e1306332bff011ebb419e9d098839ae99a01b
SMcGregor199 <shaynemcgregor1@gmail.com>
2026-04-15 09:37:50 -0400

    replace App component with Main component displaying Hello Rap World

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {createRoot} from "react-dom/client";
+const root = createRoot(document.querySelector("#root"));
+const Main = () => {
+  return (
+    <main>
+      <h1>Hello Rap World</h1>
+    </main>
+  )
+}
+root.render(
+  <Main />
 )
